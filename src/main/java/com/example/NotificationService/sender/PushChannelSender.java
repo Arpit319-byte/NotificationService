a853b0f2cad911e16264c6df9a39a492384741ae
Arpit319-byte <arpitchauhanupal@gmail.com>
2026-06-22 03:48:18 +0530

    Added the ChannelSender implementation

--- src/main/java/com/example/NotificationService/sender/PushChannelSender.java
+++ src/main/java/com/example/NotificationService/sender/PushChannelSender.java
@@ -2,19 +2,33 @@ package com.example.NotificationService.sender;
 
 import com.example.NotificationService.entity.Channel;
 import com.example.NotificationService.entity.Notification;
+import com.example.NotificationService.entity.User;
+import com.example.NotificationService.exception.ResourceNotFoundException;
+import com.example.NotificationService.repository.UserRepository;
 
 public class PushChannelSender implements ChannelSender{
 
+    private final UserRepository userRepository;
+
+    public PushChannelSender(UserRepository userRepository){
+        this.userRepository=userRepository;
+    }
+
     @Override
     public Channel getChannel() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getChannel'");
+       return Channel.PUSH;
     }
 
     @Override
     public void sendNotification(Notification notification) throws Exception {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'sendNotification'");
+        
+                
+        User user=userRepository.findById(notification.getUserId())
+                  .orElseThrow(() -> new ResourceNotFoundException("User not found with id: " + notification.getUserId()));
+
+        System.out.printf("PUSH → %s | subject: %s | body: %s%n",
+        user.getEmail(), notification.getSubject(), notification.getMessage());
+                  
     }
     
     
